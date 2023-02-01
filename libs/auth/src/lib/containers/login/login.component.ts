5a16b4dd9041ed5cd097f23f9c708e99982b62b6
alexle-developer <alex.le.developer@gmail.com>
2023-02-01 16:18:34 -0600

    add login function  to container component

--- libs/auth/src/lib/containers/login/login.component.ts
+++ libs/auth/src/lib/containers/login/login.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'angular-redux-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  login(authenticate: any) {
+    console.log(authenticate);
+  }
+
+}
