3ef96b87564482c7c1ab9cf209be7aaa1e1f6bb2
alexle-developer <alex.le.developer@gmail.com>
2023-02-06 18:49:29 -0600

    import common material componetns & re-export them

--- libs/material/src/lib/material.module.ts
+++ libs/material/src/lib/material.module.ts
@@ -1,7 +1,47 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTableModule} from '@angular/material/table';
+import {MatSelectModule} from '@angular/material/select';
+
+// Add all the common material components and re-export them
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    FlexLayoutModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatTableModule,
+    MatSelectModule
+  ],
+  exports: [
+    FlexLayoutModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatTableModule,
+    MatSelectModule
+  ]
 })
-export class MaterialModule {}
+export class MaterialModule { }
