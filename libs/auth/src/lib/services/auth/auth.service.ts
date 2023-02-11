0eaa53edc0b0d3a2df9e6fb896e948ec4279a6c6
alexle-developer <alex.le.developer@gmail.com>
2023-02-11 17:40:11 -0600

    add material tool bar & BehaviorSubject to service

--- libs/auth/src/lib/services/auth/auth.service.ts
+++ libs/auth/src/lib/services/auth/auth.service.ts
@@ -1,12 +1,18 @@
 import {Authenticate, User} from '@angular-redux/data-models';
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {BehaviorSubject, Observable, tap} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  // To initialize the BehaviorSubject with null value
+  // you must define the type as User | null.  Otherwise,
+  // you will get the 'null is not assignable to parameter of type User'
+  private userSubject$ = new BehaviorSubject<User | null>(null);
+  user$ = this.userSubject$.asObservable();
 
   constructor (private httpClient: HttpClient) { }
 
@@ -16,8 +22,10 @@ export class AuthService {
 
   // add types to auth service - replace unknown with User
   login(authenticate: Authenticate): Observable<User> {
-    return this.httpClient.post<User>(
-      'http://localhost:3000/login',
-      authenticate);
+    return this.httpClient
+      .post<User>('http://localhost:3000/login', authenticate)
+      .pipe(
+        tap((user: User) => this.userSubject$.next(user))
+      );
   }
 }
--- libs/layout/src/lib/layout.module.ts
+++ libs/layout/src/lib/layout.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './containers/layout/layout.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LayoutComponent} from './containers/layout/layout.component';
+import {MaterialModule} from '@angular-redux/material';
+import {RouterModule} from '@angular/router';
 
+
+// Add the MaterialModule and Router Module
+// Export the Layout Component out of the module;
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule, RouterModule],
   declarations: [LayoutComponent],
+  exports: [LayoutComponent]
 })
-export class LayoutModule {}
+export class LayoutModule { }
