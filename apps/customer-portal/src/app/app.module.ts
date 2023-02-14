3123e4890ad683784eeddf5e9630ab9edb549a5a
alexle-developer <alex.le.developer@gmail.com>
2023-02-14 11:23:33 -0600

    add a new default route to products

--- apps/customer-portal/src/app/app.module.ts
+++ apps/customer-portal/src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { LayoutModule } from '@angular-redux/layout';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import {LayoutModule} from '@angular-redux/layout';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
 
 // @angular-redux = [libs/auth/src/index.ts] in tsconfig.base.json
-import { authRoutes, AuthModule } from '@angular-redux/auth';
+import {authRoutes, AuthModule} from '@angular-redux/auth';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,17 +17,27 @@ import { authRoutes, AuthModule } from '@angular-redux/auth';
     LayoutModule,
     RouterModule.forRoot(
       [
-        { path: 'auth', children: authRoutes },
+        {
+          path: '',
+          pathMatch: 'full',
+          redirectTo: 'products'
+        },
+        {
+          path: 'auth',
+          children: authRoutes
+        },
         {
           path: 'products',
           loadChildren: () =>
             import('@angular-redux/products').then((m) => m.ProductsModule),
         },
       ],
-      { initialNavigation: 'enabledBlocking' }
+      {
+        initialNavigation: 'enabledBlocking'
+      }
     ),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
