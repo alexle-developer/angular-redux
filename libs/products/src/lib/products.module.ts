b55f3a5209dce451db69d33576bbf9ab3afff282
alexle-developer <alex.le.developer@gmail.com>
2023-02-14 11:33:41 -0600

    add route for products in ProductModule

--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { productsRoutes } from './lib.routes';
-import { ProductsComponent } from './containers/products/products.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {ProductsComponent} from './containers/products/products.component';
+// we could have declare the routes here
+// import {productsRoutes} from './lib.routes';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(productsRoutes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([{path: '', component: ProductsComponent}])
+  ],
   declarations: [ProductsComponent],
 })
-export class ProductsModule {}
+export class ProductsModule { }
