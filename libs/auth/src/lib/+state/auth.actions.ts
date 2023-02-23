c3bad2b3d6b7af3a519ddf18b899ae4d26c8ec2a
alexle-developer <alex.le.developer@gmail.com>
2023-02-22 19:38:28 -0600

    add Login Action Creators

--- libs/auth/src/lib/+state/auth.actions.ts
+++ libs/auth/src/lib/+state/auth.actions.ts
@@ -1,14 +1,39 @@
-import { createAction, props } from '@ngrx/store';
-import { AuthEntity } from './auth.models';
+import {Authenticate, User} from '@angular-redux/data-models';
+import {createAction, props} from '@ngrx/store';
+import {AuthEntity} from './auth.models';
+
 
 export const initAuth = createAction('[Auth Page] Init');
 
 export const loadAuthSuccess = createAction(
   '[Auth/API] Load Auth Success',
-  props<{ auth: AuthEntity[] }>()
+  props<{auth: AuthEntity[];}>()
 );
 
 export const loadAuthFailure = createAction(
   '[Auth/API] Load Auth Failure',
-  props<{ error: any }>()
+  props<{error: any;}>()
+);
+
+
+// add login action creators
+export enum AuthActionTypes {
+  Login = '[Auth Page] Login',
+  LoginSuccess = '[Auth API] Login Success',
+  LoginFail = '[Auth API] Login Fail'
+}
+
+export const login = createAction(
+  AuthActionTypes.Login,
+  props<{payload: Authenticate;}>()
+);
+
+export const loginSuccess = createAction(
+  AuthActionTypes.LoginSuccess,
+  props<{payload: User;}>()
+);
+
+export const loginFailure = createAction(
+  AuthActionTypes.LoginFail,
+  props<{payload: any;}>()
 );
